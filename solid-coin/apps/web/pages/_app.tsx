2c2384dbffa5014554bf4c9455f7659a6550b89c
iorran <iorranpt@gmail.com>
2021-10-12 17:40:15 +0000

    Removing teplate css

--- solid-coin/apps/web/pages/_app.tsx
+++ solid-coin/apps/web/pages/_app.tsx
@@ -1,18 +1,15 @@
 import { AppProps } from 'next/app';
-import Head from 'next/head';
-import './styles.css';
+import Head from 'next/head'; 
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to web!</title>
+        <title>Solid Coin</title>
       </Head>
       <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to web!</h1>
+        <header className="flex"> 
+          <h1>Keep Investing!</h1>
         </header>
         <main>
           <Component {...pageProps} />
