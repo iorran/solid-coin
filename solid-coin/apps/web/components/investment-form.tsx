5f6d86e0001121b74fbf3453af271f87fb6ab75d
iorran <iorranpt@gmail.com>
2021-10-16 02:52:16 +0000

    simple form validation

--- solid-coin/apps/web/components/investment-form.tsx
+++ solid-coin/apps/web/components/investment-form.tsx
@@ -1,12 +1,13 @@
 import { useForm } from "react-hook-form";
 import { Box, TextField, Button } from '@mui/material';
+import {isBefore, parseISO} from 'date-fns';
 
 import { useAtom } from 'jotai';
 import { investmentFormAtom, IInvestmentForm } from '../utils/atoms'; 
  
 export const InvestmentForm = () => { 
     const [investment, compute] = useAtom(investmentFormAtom);   
-    const { register, handleSubmit } = useForm<IInvestmentForm>({ mode: 'onChange', defaultValues: investment });
+    const { register, handleSubmit, formState:{ errors } } = useForm<IInvestmentForm>({ mode: 'onChange', defaultValues: investment });
 
     const onSubmit = (data: IInvestmentForm) => {  
         compute(data);
@@ -30,7 +31,16 @@ export const InvestmentForm = () => {
           onSubmit={() => onSubmit({} as IInvestmentForm)}
         >   
             <TextField 
-                {...register("startDate", { required: true })}  
+                error={!!errors?.startDate?.message} 
+                helperText={errors?.startDate?.message}
+                {...register("startDate", { 
+                    required: true,
+                    validate: {
+                        isBefore: v => { 
+                            return isBefore(new Date(parseISO(v)), new Date()) || 'Date must be before today' 
+                        }
+                    }
+                 })}  
                 label="Since"
                 type="date"
                 InputLabelProps={{ shrink: true }}
@@ -39,7 +49,7 @@ export const InvestmentForm = () => {
                 {...register("money", { required: true })}  
                 label="Investment"
                 InputLabelProps={{ shrink: true }}
-            />   
+            />    
             <Button variant="contained" onClick={recalculate}>Check</Button>
         </Box>
     );
--- solid-coin/apps/web/components/profit-panel.tsx
+++ solid-coin/apps/web/components/profit-panel.tsx
@@ -3,17 +3,18 @@ import { Box, Typography } from '@mui/material';
 
 import { useAtom } from 'jotai';
 import { profitAtom } from '../utils/atoms'; 
-import { formatCurrency  } from '../utils/formatters';
+import { formatCurrency } from '../utils/formatters';
  
 export const ProfitPanel = () => { 
     const [profit, ] = useAtom(profitAtom);     
 
+
     if(!profit) return null;
 
     return (
-        <Box>   
-            <Typography variant="h6">You just lose: {formatCurrency(profit)}</Typography>
-            <Typography variant="caption">Perhaps you won, who knows ?</Typography>
+        <Box>    
+            <Typography variant="h6">You won: {formatCurrency(profit)}</Typography>
+            <Typography variant="caption">Did you invest, right?</Typography>
         </Box>
     );
 }
